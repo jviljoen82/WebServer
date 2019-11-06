0899a567bc3f17f15bcdf08f6e35dc443a78814d
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-11-06 08:43:12 +0200

    Starting app in angular

---
+++ public/js/zumiapp.js
@@ -0,0 +1,8 @@
+const ZumisApp = angular.module('ZumisWorldApp', []);
+
+ZumisApp.controller('AppCtrl', ($scope) => {
+
+});
+ZumisApp.directive('MainBody', () => {
+    template: "<img src=\"images/Kazumi.jpeg\" />"
+});
