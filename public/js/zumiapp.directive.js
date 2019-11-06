4ce48d3595bceb388f43465e0f4685cb37cecb4e
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-11-06 09:59:41 +0200

    moved heading

--- public/js/zumiapp.directive.js
+++ public/js/zumiapp.directive.js
@@ -1,8 +1,9 @@
 ZumisApp.directive('mainBody', () => {
     return {
         template: `<div layout="column" layout-align="start center">
+                        <h2 class="md-display-2"> Kazumi says...WAIT!!! </h2>
                         <img src='../images/Kazumi.jpeg' alt='' />
-                        <h2> Kazumi says...WAIT!!! </h2>
+                        
                         <img src='../images/Under-Construction.jpg' alt='' />
                     
                         <p> the realm is already up and running... the website will be ready in a little bit</p>
