410533321b09c6d6f38dc27aa43563c531a15cad
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-11-15 09:27:22 +0200

    Finished WoW download section

--- public/js/default/components/downloadContainer/controller.js
+++ public/js/default/components/downloadContainer/controller.js
@@ -13,6 +13,11 @@ class DownloadCtrl {
             template: `<md-dialog aria-label="Download dialog">
                             <md-dialog-content>
                                <H3>Download World of Warcraft - Wrath of the Lich King - v3.3.5a</H3>
+                                <div class="whiteBackground">
+                                    <a href="../../../../../data/WoW335aMac.zip">World of Warcraft - Wrath of the Lich King v3.3.5a  (MAC)</a>
+                                    <br>
+                                    <a href="../../../../../data/WoW335aWin.zip">World of Warcraft - Wrath of the Lich King v3.3.5a  (Win)</a>
+                                </div>
                             </md-dialog-content>
                             <md-dialog-actions>
                                 <md-button ng-click="$dlCtrl.closeDialog()" class="md-button">Close</md-button>
