7524806df51c72d3ea56fe202f6a2adc0c52ffb9
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-11-15 14:04:13 +0200

    doh

--- public/js/chatter/client.js
+++ public/js/chatter/client.js
@@ -81,9 +81,9 @@ function sendMessage(message) {
       'Access-Control-Allow-Origin': '*'
     },
     data: message,
-    success: () => {/*setTimeout( () => {
-        latestMsg().then(autoScroll);
-      }, 1500); */
+    success: () => {setTimeout( () => {
+        socket.sockets.emit('message');
+      }, 1500);
     }
   });
 }
