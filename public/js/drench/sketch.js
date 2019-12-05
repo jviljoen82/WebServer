87e0429f67df8e507d133610993d8eae09f593e4
Johan Viljoen <cj_viljoen@mweb.co.za>
2019-12-05 15:07:37 +0200

    some refactor:

--- public/js/drench/sketch.js
+++ public/js/drench/sketch.js
@@ -30,134 +30,134 @@ let xmasMsg = 'Yaaaargh! Wasn\'t that fun? You\'ve found yer treasure - and it t
 
 
 function make2DArray(gridSize) {
-  let arr = new Array(gridSize);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(gridSize);
-  }
-  return arr;
+    let arr = new Array(gridSize);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(gridSize);
+    }
+    return arr;
 }
 
 function showScore() {
-  const scoreString = 'Your score: ' + score;
-  fill('#1199FF');
-  textStyle(BOLD);
-  textSize(15);
-  textAlign(CENTER);
-  text(scoreString, 180, 395, 200, 15);
-  textSize(0);
+    const scoreString = 'Your score: ' + score;
+    fill('#1199FF');
+    textStyle(BOLD);
+    textSize(15);
+    textAlign(CENTER);
+    text(scoreString, 180, 395, 200, 15);
+    textSize(0);
 }
 
 function showClickCount() {
-  const clicksString = 'Clicks Remain: ' + clicksLeft;
-  if (clicksLeft >= setClicks / 2) {
-    fill('#009900');
-  } else if ((clicksLeft >= setClicks / 4) && (clicksLeft < setClicks / 2)) {
-    fill('#FFC300');
-  } else fill('#FF0000');
-  textStyle(BOLD);
-  textSize(15);
-  textAlign(CENTER);
-  text(clicksString, 180, 365, 200, 15);
-  textSize(0);
+    const clicksString = 'Clicks Remain: ' + clicksLeft;
+    if (clicksLeft >= setClicks / 2) {
+        fill('#009900');
+    } else if ((clicksLeft >= setClicks / 4) && (clicksLeft < setClicks / 2)) {
+        fill('#FFC300');
+    } else fill('#FF0000');
+    textStyle(BOLD);
+    textSize(15);
+    textAlign(CENTER);
+    text(clicksString, 180, 365, 200, 15);
+    textSize(0);
 }
 
 function btnSetup(currentBtn, index) {
-  let btnColor = color(red[index], green[index], blue[index]);
-  if (index <= 2) {
-    currentBtn.x = 15 + (index * 50);
-    currentBtn.y = 310;
-  } else {
-    currentBtn.x = 15 + ((index - 3) * 50);
-    currentBtn.y = 360;
-  }
-  currentBtn.height = buttonSize;
-  currentBtn.width = buttonSize;
-  currentBtn.color = btnColor;
-  currentBtn.cornerRadius = 10;
-  currentBtn.strokeWeight = 2;
-  currentBtn.stroke = '#000000';
-  currentBtn.textColor = btnColor;
+    let btnColor = color(red[index], green[index], blue[index]);
+    if (index <= 2) {
+        currentBtn.x = 15 + (index * 50);
+        currentBtn.y = 310;
+    } else {
+        currentBtn.x = 15 + ((index - 3) * 50);
+        currentBtn.y = 360;
+    }
+    currentBtn.height = buttonSize;
+    currentBtn.width = buttonSize;
+    currentBtn.color = btnColor;
+    currentBtn.cornerRadius = 10;
+    currentBtn.strokeWeight = 2;
+    currentBtn.stroke = '#000000';
+    currentBtn.textColor = btnColor;
 }
 
 function setChangeAble() {
-  // set which cells are changeable
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      if (grid[i][j].changeAble) {
-        if (i === 0 && j >= 0 && j <= gridSize-1) {
-          if (j != gridSize-1 && grid[i][j + 1].colorCode === grid[0][0].colorCode) grid[i][j + 1].changeAble = true;
-          if (grid[i + 1][j].colorCode === grid[0][0].colorCode) grid[i + 1][j].changeAble = true;
-          if (j > 0  && (grid[i][j - 1].colorCode === grid[0][0].colorCode)) grid[i][j - 1].changeAble = true;
+    // set which cells are changeable
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            if (grid[i][j].changeAble) {
+                if (i === 0 && j >= 0 && j <= gridSize-1) {
+                    if (j !== gridSize-1 && grid[i][j + 1].colorCode === grid[0][0].colorCode) grid[i][j + 1].changeAble = true;
+                    if (grid[i + 1][j].colorCode === grid[0][0].colorCode) grid[i + 1][j].changeAble = true;
+                    if (j > 0  && (grid[i][j - 1].colorCode === grid[0][0].colorCode)) grid[i][j - 1].changeAble = true;
+                }
+                if (i > 0 && i <= gridSize-1 && j >= 0 && j <= gridSize-1) {
+                    if (j !== gridSize-1 && grid[i][j + 1].colorCode === grid[0][0].colorCode) grid[i][j + 1].changeAble = true;
+                    if (i !== gridSize-1 && grid[i + 1][j].colorCode === grid[0][0].colorCode) grid[i + 1][j].changeAble = true;
+                    if (j > 0  && (grid[i][j - 1].colorCode === grid[0][0].colorCode)) grid[i][j - 1].changeAble = true;
+                    if (grid[i - 1][j].colorCode === grid[0][0].colorCode) grid[i - 1][j].changeAble = true;
+                }
+                if (i === gridSize-1 && j >= 0 && j < gridSize-1) {
+                    if (j !== gridSize-1 && grid[i][j + 1].colorCode === grid[0][0].colorCode) grid[i][j + 1].changeAble = true;
+                    if (grid[i - 1][j].colorCode === grid[0][0].colorCode) grid[i - 1][j].changeAble = true;
+                    if (j > 0 && (grid[i][j - 1].colorCode === grid[0][0].colorCode)) grid[i][j - 1].changeAble = true;
+                }
+            }
         }
-        if (i > 0 && i <= gridSize-1 && j >= 0 && j <= gridSize-1) {
-          if (j != gridSize-1 && grid[i][j + 1].colorCode === grid[0][0].colorCode) grid[i][j + 1].changeAble = true;
-          if (i != gridSize-1 && grid[i + 1][j].colorCode === grid[0][0].colorCode) grid[i + 1][j].changeAble = true;
-          if (j > 0  && (grid[i][j - 1].colorCode === grid[0][0].colorCode)) grid[i][j - 1].changeAble = true;
-          if (grid[i - 1][j].colorCode === grid[0][0].colorCode) grid[i - 1][j].changeAble = true;
-        }
-        if (i === gridSize-1 && j >= 0 && j < gridSize-1) {
-          if (j != gridSize-1 && grid[i][j + 1].colorCode === grid[0][0].colorCode) grid[i][j + 1].changeAble = true;
-          if (grid[i - 1][j].colorCode === grid[0][0].colorCode) grid[i - 1][j].changeAble = true;
-          if (j > 0 && (grid[i][j - 1].colorCode === grid[0][0].colorCode)) grid[i][j - 1].changeAble = true;
-        }
-      }
     }
-  }
 }
 
 function drawUI() {
-  //draw grid once
-  background(255);
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      grid[i][j].show();
+    //draw grid once
+    background(255);
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            grid[i][j].show();
+        }
     }
-  }
-  showScore();
-  showClickCount();
+    showScore();
+    showClickCount();
 }
 
 function setup() {
-  createCanvas(380, 420);
-  grid = make2DArray(gridSize);
+    createCanvas(380, 420);
+    grid = make2DArray(gridSize);
 
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      grid[i][j] = new Cell(i, j, w);
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            grid[i][j] = new Cell(i, j, w);
         }
-  }
+    }
 
-  //set first cell changeAble
-  grid[0][0].changeAble = true;
-  setChangeAble();
-  drawUI();
+    //set first cell changeAble
+    grid[0][0].changeAble = true;
+    setChangeAble();
+    drawUI();
 
-  // button setup
-  for (ind = 0; ind <= 5; ind++){
-    btnSetup(btnList[ind], ind);
-  }
+    // button setup
+    for (ind = 0; ind <= 5; ind++){
+        btnSetup(btnList[ind], ind);
+    }
 
-  //reset btn
-  resetBtn.x = 180;
-  resetBtn.y = 300;
-  resetBtn.color = '#C0C0C0';
-  resetBtn.cornerRadius = 10;
-  resetBtn.strokeWeight = 2;
-  resetBtn.stroke = '#808080';
-  resetBtn.textColor = '#808080';
-  resetBtn.text = 'RESET';
+    //reset btn
+    resetBtn.x = 180;
+    resetBtn.y = 300;
+    resetBtn.color = '#C0C0C0';
+    resetBtn.cornerRadius = 10;
+    resetBtn.strokeWeight = 2;
+    resetBtn.stroke = '#808080';
+    resetBtn.textColor = '#808080';
+    resetBtn.text = 'RESET';
 
 }
 
 function draw() {
-  // draw button panel with buttons
-  fill('#C0C0C0');
-  stroke('#808080');
-  rect(5, 300, 160, 110);
-  for (btnInd = 0; btnInd <= 5; btnInd++) {
-    btnList[btnInd].draw();
-  }
-  resetBtn.draw();
+    // draw button panel with buttons
+    fill('#C0C0C0');
+    stroke('#808080');
+    rect(5, 300, 160, 110);
+    for (btnInd = 0; btnInd <= 5; btnInd++) {
+        btnList[btnInd].draw();
+    }
+    resetBtn.draw();
 }
 
 // *******************************
@@ -166,107 +166,107 @@ function draw() {
 
 // button1
 Button1.onOutside = () => {
-  Button1.stroke = '#000000';
+    Button1.stroke = '#000000';
 };
 Button1.onHover = () => {
-  Button1.stroke = '#FFFFFF';
+    Button1.stroke = '#FFFFFF';
 };
 
 // button2
 Button2.onOutside = () => {
-  Button2.stroke = '#000000';
+    Button2.stroke = '#000000';
 };
 Button2.onHover = () => {
-  Button2.stroke = '#FFFFFF';
+    Button2.stroke = '#FFFFFF';
 };
 
 // button3
 Button3.onOutside = () => {
-  Button3.stroke = '#000000';
+    Button3.stroke = '#000000';
 };
 Button3.onHover = () => {
-  Button3.stroke = '#FFFFFF';
+    Button3.stroke = '#FFFFFF';
 };
 
 // button4
 Button4.onOutside = () => {
-  Button4.stroke = '#000000';
+    Button4.stroke = '#000000';
 };
 Button4.onHover = () => {
-  Button4.stroke = '#FFFFFF';
+    Button4.stroke = '#FFFFFF';
 };
 
 // button5
 Button5.onOutside = () => {
-  Button5.stroke = '#000000';
+    Button5.stroke = '#000000';
 };
 Button5.onHover = () => {
-  Button5.stroke = '#FFFFFF';
+    Button5.stroke = '#FFFFFF';
 };
 
 // button6
 Button6.onOutside = () => {
-  Button6.stroke = '#000000';
+    Button6.stroke = '#000000';
 };
 Button6.onHover = () => {
-  Button6.stroke = '#FFFFFF';
+    Button6.stroke = '#FFFFFF';
 };
 
 // reset button
 resetBtn.onOutside = () => {
-  resetBtn.stroke = '#808080';
+    resetBtn.stroke = '#808080';
 };
 resetBtn.onHover = () => {
-  resetBtn.stroke = '#FFFFFF';
+    resetBtn.stroke = '#FFFFFF';
 };
 
 function checkWin() {
-  let breakCheck = false;
-  let conditionCheck = false;
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      if ((i === 0 && j != 0) || i != 0) {
-        if (grid[i][j].colorCode != grid[0][0].colorCode) {
-          conditionCheck = false;
-          breakCheck = true;
-          break;
-        } else conditionCheck = true;
-      }
+    let breakCheck = false;
+    let conditionCheck = false;
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            if ((i === 0 && j !== 0) || i !== 0) {
+                if (grid[i][j].colorCode !== grid[0][0].colorCode) {
+                    conditionCheck = false;
+                    breakCheck = true;
+                    break;
+                } else conditionCheck = true;
+            }
+        }
+        if (breakCheck) break;
     }
-    if (breakCheck) break;
-  }
-  return conditionCheck;
+    return conditionCheck;
 }
 
 function colorChange(toColor) {
-  // check if same color
-  if (toColor != grid[0][0].colorCode) { 
-    // change colours
-    for (let i = 0; i < gridSize; i++) {
-      for (let j = 0; j < gridSize; j++) {
-        if (grid[i][j].changeAble) {
-          grid[i][j].c = color(red[toColor], green[toColor], blue[toColor]);
-          grid[i][j].colorCode = toColor;
+    // check if same color
+    if (toColor !== grid[0][0].colorCode) {
+        // change colours
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+                if (grid[i][j].changeAble) {
+                    grid[i][j].c = color(red[toColor], green[toColor], blue[toColor]);
+                    grid[i][j].colorCode = toColor;
+                }
+            }
         }
-      }
-    }
-    for (recurse = 0; recurse < gridSize; recurse++) {
-      setChangeAble();
-    }
-    drawUI();
+        for (recurse = 0; recurse < gridSize; recurse++) {
+            setChangeAble();
+        }
+        drawUI();
 
-    // check win conditions and click remaining
-    if (checkWin()) {
-      score += clicksLeft * 10;
-      alert("Great, You Won! New round?");
-      if (score >= 100) console.log(xmasMsg);
-      newGame();
-    }
-    clickAmount();
+        // check win conditions and click remaining
+        if (checkWin()) {
+            score += clicksLeft * 10;
+            alert("Great, You Won! New round?");
+            if (score >= 100) console.log(xmasMsg);
+            newGame();
+        }
+        clickAmount();
 
-    // refresh UI
-    drawUI();
-  }
+        // refresh UI
+        drawUI();
+    }
 }
 
 function clickAmount() {
@@ -274,17 +274,17 @@ function clickAmount() {
     console.log(clicksLeft);
 
     if(clicksLeft === 0) {
-      alert('Game Over!');
-      score = 0;
-      newGame();
+        alert('Game Over!');
+        score = 0;
+        newGame();
     }
 }
 
 function newGame() {
-  if (checkWin()) {
-    clicksLeft = setClicks + 1;
-  } else clicksLeft = setClicks;
-  setup();
+    if (checkWin()) {
+        clicksLeft = setClicks + 1;
+    } else clicksLeft = setClicks;
+    setup();
 }
 
 // ***************************************
@@ -293,57 +293,57 @@ function newGame() {
 
 // button1
 Button1.onPress = function () {
-  // do nothing
+    // do nothing
 };
 Button1.onRelease = function () {
-  colorChange(0);
+    colorChange(0);
 };
 
 // button2
 Button2.onPress = function () {
-  // do nothing
+    // do nothing
 };
 Button2.onRelease = function () {
-  colorChange(1);
+    colorChange(1);
 };
 
 // button3
 Button3.onPress = function () {
-  // do nothing
+    // do nothing
 };
 Button3.onRelease = function () {
-  colorChange(2);
+    colorChange(2);
 };
 
 // button4
 Button4.onPress = function () {
-  // do nothing
+    // do nothing
 };
 Button4.onRelease = function () {
-  colorChange(3);
+    colorChange(3);
 };
 
 // button5
 Button5.onPress = function () {
-  // do nothing
+    // do nothing
 };
 Button5.onRelease = function () {
-  colorChange(4);
+    colorChange(4);
 };
 
 // button6
 Button6.onPress = function () {
-  // do nothing
+    // do nothing
 };
 Button6.onRelease = function () {
-  colorChange(5);
+    colorChange(5);
 };
 
 // reset button
 resetBtn.onPress = function () {
-  // do nothing
+    // do nothing
 };
 resetBtn.onRelease = function () {
-  score = 0;
-  newGame();
+    score = 0;
+    newGame();
 };
