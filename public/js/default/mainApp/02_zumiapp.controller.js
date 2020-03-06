3bdad0b4d0a214f302b04cadea4fbc7f19e08539
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-03-06 09:45:36 +0200

    login dialog

--- public/js/default/mainApp/02_zumiapp.controller.js
+++ public/js/default/mainApp/02_zumiapp.controller.js
@@ -1,15 +1,30 @@
 class AppCtrl {
-    constructor($scope, $window) {
+    constructor($scope, $mdDialog) {
         this.scope = $scope;
-        this.window = $window;
-        this.scope = (getName) => {
-            this.window.prompt('Username: ', getName);
-            userCreds.name = getName;
-        }
+        this.mdDialog = $mdDialog;
     }
 
     $onInit() {
+        this.mdDialog.show ({
+            clickOutsideToClose: false,
+            scope: this.scope,
+            preserveScope: true,
+            template: `<md-dialog aria-label="Login">
+                            <md-dialog-content>
+                                <md-input-container>
+                                      <label>NAME</label>
+                                      <input type="text" ng-model="{{::userCreds.name}}" required="" md-maxlength="10">
+                                </md-input-container>
+                            </md-dialog-content>
+                            <md-dialog-actions>
+                                <md-button ng-click="$appCtrl.closeDialog()" class="md-button">OK</md-button>
+                            </md-dialog-actions>
+                        </md-dialog>`
+        });
+    }
 
+    closeDialog() {
+        this.mdDialog.hide();
     }
 }
 
