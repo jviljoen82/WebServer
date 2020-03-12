7c576ac54760f144657d45a243b6aded518eb18d
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-03-12 09:43:08 +0200

    trying something

--- public/js/default/components/sideChatPanel/controller.js
+++ public/js/default/components/sideChatPanel/controller.js
@@ -3,31 +3,179 @@ class ChatPanel {
         this.scope = $scope;
         this.mdDialog = $mdDialog;
         this.user = userCreds;
+        this.msgDiv = document.getElementById('messages');
+        this.socket = io.connect('http://zumisworld.ga:8080');
+        this.chatServerUrl = 'http://zumisworld.ga:8070';
+        this.waitValue = 500;
+
+    }
+
+    $onInit() {
+        this.socket.on('message', this.scope.bind(this.updateMsgs()));
     }
 
     showChatPanel() {
-
-        console.log("Chat Panel Open");
         this.mdDialog.show ({
             clickOutsideToClose: false,
             scope: this.scope,
             preserveScope: true,
             template: `<md-dialog aria-label="Download dialog">
                             <md-dialog-content>
-                                <iframe id="chatFrame" allow-same-origin class="sidePanel" sandbox="allow-scripts" src="../../../../chat.html"></iframe>
+                                <div class="container">
+                                  <br>
+                                  <div id="messages" class="chatwindow row">
+                                  </div>
+                                  <div class="jumbotron row">
+                                    <h4>Send Message</h4>
+                                    <br>
+                                    <input id = "name" class="form-control" placeholder="Name" readonly >
+                                    <br>
+                                    <textarea id = "message" class="form-control" placeholder="Your Message Here"></textarea>
+                                    <br>
+                                    <button class="btn btn-success" ng-click="$chatCtrl.sendAndClear()">Send</button>
+                                  </div>
+                                </div>
                             </md-dialog-content>
                             <md-dialog-actions>
                                 <md-button ng-click="$chatCtrl.closeDialog()" class="md-button">Close</md-button>
                             </md-dialog-actions>
                         </md-dialog>`
         });
-        console.log("username: " + this.user.name);
-        let chatFrame = window.frames['chatFrame'];
-        chatFrame.setName(this.user.name);
+        this.getMessages().then();
+        this.constructor.setName(this.user.name);
     }
 
     closeDialog() {
         this.mdDialog.hide();
+    }
+
+    static setName(username) {
+        console.log("Setting username: " + username);
+        document.getElementById('name').value = username;
+    }
+
+    autoScroll() {
+        this.msgDiv.scrollTop = this.msgDiv.scrollHeight;
+    }
+
+    sendAndClear() {
+        this.sendMessage({
+            id: 999,
+            name: document.getElementById('name').value,
+            message: document.getElementById('message').value
+        });
+        document.getElementById('message').value = '';
+    }
+
+    updateMsgs() {
+        this.latestMsg().then();
+    }
+
+    static addMessages(message) {
+        document.getElementById('messages').innerHTML += `
+        <div class="col-md-12">    
+          <h4> ${message.name} </h4>
+          <p>  ${message.message} </p>
+        </div>
+      `;
+    }
+
+    async getMessages() {
+        await sleep(this.waitValue);
+        document.getElementById('messages').innerHTML = '';
+        $.ajax({
+            type: 'GET',
+            url: this.chatServerUrl+'/call',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            data: { id: 999 },
+            success: (data) => {
+                data.forEach(addMessages);
+                this.autoScroll();
+            },
+            error: (error) => {
+                console.log('server error: ', error);
+                console.log('using local db');
+                $.ajax({
+                    type: 'GET',
+                    url: 'http://localhost:8070/call',
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    data: {id: 999},
+                    success: (data) => {
+                        data.forEach(addMessages);
+                        this.autoScroll();
+                    }
+                })
+            }
+        });
+    }
+
+    async latestMsg() {
+        await this.sleep(this.waitValue);
+        $.ajax({
+            type: 'GET',
+            url: this.chatServerUrl+'/call',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            data: { id: 999 },
+            success: (data) => {
+                const lastMsg = data[data.length - 1];
+                this.constructor.addMessages(lastMsg);
+                this.autoScroll();
+            },
+            error: (error) => {
+                console.log('server error: ', error);
+                console.log('using local db');
+                $.ajax({
+                    type: 'GET',
+                    url: 'http://localhost:8070/call',
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    data: { id: 999 },
+                    success: (data) => {
+                        const lastMsg = data[data.length - 1];
+                        this.constructor.addMessages(lastMsg);
+                        this.autoScroll();
+                    }
+                })
+            }
+        });
+    }
+
+    sendMessage(message) {
+        $.ajax({
+            type: 'POST',
+            url: this.chatServerUrl+'/msg',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            data: message,
+            success: () => {},
+            error: (error) => {
+                console.log('server error: ', error);
+                console.log('using local db');
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://localhost:8070/msg',
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    data: message,
+                    success: () => {}
+                })
+            }
+        });
+    }
+
+    sleep(ms){
+        return new Promise(resolve => {
+            setTimeout(resolve,ms)
+        })
     }
 }
 
