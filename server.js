0a6077660bbc701d69077b26a5b21b4ab3aeb842
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-03-12 10:45:16 +0200

    git se push

--- server.js
+++ server.js
@@ -66,9 +66,6 @@ async function doUpdate() {
     await sleep(5000);
     console.log('Pulling updated Data');
     const gitPull = executor('git pull');
-    gitPull.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
     await sleep(5000);
     console.log('Rebuilding Front-end');
     const gulper = executor('npm run gulp');
