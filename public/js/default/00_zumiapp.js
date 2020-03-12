f33370840acf5a9a78ec070877bee85800862048
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-03-12 11:12:08 +0200

    fokker

--- public/js/default/00_zumiapp.js
+++ public/js/default/00_zumiapp.js
@@ -2,4 +2,5 @@ const ZumisApp = angular.module('ZumisWorldApp', ['ngMaterial', 'ngMessages'])
     .config(($mdThemingProvider) => {
         $mdThemingProvider.theme('default').dark();
     });
+
 ZumisApp.value('userCreds', { name: '', password: ''});
