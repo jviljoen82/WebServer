5b09c8dddbdc74c9e8a88da411db977d172e7b6c
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-04-01 09:04:35 +0200

    remove button

--- public/js/default/components/sideChatPanel/controller.js
+++ public/js/default/components/sideChatPanel/controller.js
@@ -20,40 +20,9 @@ class ChatPanel {
                             <br>
                             <md-dialog-actions>
                                 <md-button ng-click="$chatCtrl.closeDialog()" class="md-button">Close</md-button>
-                                <md-button aria-label="setupBtn" id="setupBtn" ng-click="$chatCtrl.setupData()">Login</md-button>
                             </md-dialog-actions>
                         </md-dialog>`
         });
-    }
-
-    setupData() {
-        if (this.user.name != null && this.user.name !== '' && this.user.name !== 'undefined') {
-            let iframeChat = document.getElementById('chatFrame');
-            let iframeDoc = iframeChat.contentDocument;
-            let userNameControl = iframeDoc.getElementById('name');
-            userNameControl.value = this.user.name;
-            iframeDoc.getElementById('send').disabled = false;
-            document.getElementById('setupBtn').disabled = true;
-        } else {
-            /*
-            this.mdDialog.show ({
-                clickOutsideToClose: false,
-                scope: this.scope,
-                preserveScope: true,
-                template: `<md-dialog aria-label="Login">
-                            <md-dialog-content>
-                                <md-input-container>
-                                      <label>NAME</label>
-                                      <input type="text" ng-model="inputName" required="" md-maxlength="10">
-                                </md-input-container>
-                            </md-dialog-content>
-                            <md-dialog-actions>
-                                <md-button ng-click="$appCtrl.closeDialog()" class="md-button">OK</md-button>
-                            </md-dialog-actions>
-                        </md-dialog>`
-            });
-            */
-        }
     }
 
     closeDialog() {
