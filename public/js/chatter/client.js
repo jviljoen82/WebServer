f098feca67461b490f6163ec74ffc58880b9b9df
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-04-01 08:51:15 +0200

    prompt for username in chat

--- public/js/chatter/client.js
+++ public/js/chatter/client.js
@@ -4,6 +4,9 @@ const chatServerUrl = 'http://zumisworld.ga:8070';
 const waitValue = 500;
 
 window.addEventListener('load', (ev) => {
+  let username = '';
+  prompt('Username: ', username);
+  document.getElementById('name').value = username;
   if (document.getElementById('name').value == null || document.getElementById('name').value === 'undefined' || document.getElementById('name').value === '') {
     document.getElementById('send').disabled = true;
   }
