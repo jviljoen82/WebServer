d2d8166d7bf9a52050683e8485849bd9a1482364
Johan Viljoen <cj_viljoen@mweb.co.za>
2020-05-22 08:41:01 +0200

    cleanup

--- public/js/default/00_zumiapp.js
+++ public/js/default/00_zumiapp.js
@@ -2,5 +2,3 @@ const ZumisApp = angular.module('ZumisWorldApp', ['ngMaterial', 'ngMessages'])
     .config(($mdThemingProvider) => {
         $mdThemingProvider.theme('default').dark();
     });
-
-ZumisApp.value('userCreds', { name: '', password: ''});
